7fc620a239cb5ccde97ab553a7b9b4f4851d460f
Gowtham0033 <126399059+Gowtham0033@users.noreply.github.com>
2025-07-02 11:18:34 +0530

    Time: 1 ms (83.79%), Space: 41.8 MB (90.53%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,32 +1,24 @@
-
 class Solution {
     public List<List<Integer>> generate(int n) {
         List<List<Integer>> li = new ArrayList<>();
-        if (n == 1)
-            return List.of(List.of(1));
-        if (n == 2)
-            return List.of(List.of(1), List.of(1, 1));
+        if (n == 0)
+            return li;
 
-        List<Integer> l = new ArrayList<>();
-        l.add(1);
-        li.add(new ArrayList<>(l));
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        li.add(firstRow);
 
-        l.add(1);
-        li.add(new ArrayList<>(l));
-
-        while (n-- > 2) {
-            List<Integer> l1 = new ArrayList<>();
-            List<Integer> l2 = new ArrayList<>(li.get(li.size() - 1));
-
-            l1.add(1);
-            int i = 0;
-            while (i < l2.size() - 1) {
-                l1.add(l2.get(i) + l2.get(i + 1));
-                i++;
+        for (int i = 1; i < n; i++) {
+            List<Integer> prevRow = li.get(i - 1);
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j < i; j++) {
+                newRow.add(prevRow.get(j - 1) + prevRow.get(j));
             }
-            l1.add(1);
-            li.add(l1);
+            newRow.add(1);
+            li.add(newRow);
         }
+
         return li;
     }
 }
